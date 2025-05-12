c415f3ec571afe165c3f3bd175221d00d23cc6c7
mdt516 <mtindall516@gmail.com>
2025-05-12 18:09:36 -0500

    init commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const add = document.getElementsByName()
+
+
+
+function sum(num1, num2)
+{
+    return num1 + num2;
+}
+
+function sub(num1, num2)
+{
+    return num1 - num2;
+}
+
+function mult(num1, num2)
+{
+    return num1 * num2;
+}
+
+function div(num1, num2)
+{
+    return num1 / num2;
+}
