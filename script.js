2c2b9adeb3e821261060d344e527fd634ff5b502
mdt516 <mtindall516@gmail.com>
2025-05-12 18:30:13 -0500

    added number buttons and began addition function

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-const add = document.getElementsByName()
+let output = document.getElementById("output");
+
+//Initialzing buttons and adding functionality
+const zero = document.getElementsByName("0");
+const one = document.getElementsByName("1");
+const two = document.getElementsByName("2");
+const three = document.getElementsByName("3");
+const four = document.getElementsByName("4");
+const five = document.getElementsByName("5");
+const six = document.getElementsByName("6");
+const seven = document.getElementsByName("7");
+const eight = document.getElementsByName("8");
+const nine = document.getElementsByName("9");
 
 
+//Addition
+const additionButton = document.getElementById("addition");
+additionButton.addEventListener("click", sum);
 
 function sum(num1, num2)
 {
-    return num1 + num2;
+    output.innerHTML = num1 + num2;
 }
 
 function sub(num1, num2)
